cd0fbb74b146bacd5461105f66d487a81d9e891a
Sivadhas <sivadhas007@gmail.com>
2024-01-13 13:54:37 +0530

    changed logic for w6 ref-2

--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useCallback,useRef } from 'react';
+import { useEffect } from 'react';
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 export function Assignment2() {
     const [, forceRender] = useState(0);
 
-    let count=useRef(1);
+    let count=useRef(0);
     const handleReRender = () => {
-        // Update state to force re-render
-        count.current=count.current+1;
         forceRender(Math.random());
     };
-
+    // use effect runs only after component renders :) , so 
+    useEffect(()=>{
+        count.current=count.current+1;
+    })
     return (
         <div>
             <p>This component has rendered {count.current} times.</p>
